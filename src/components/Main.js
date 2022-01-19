8fd73bfdcb34c3991c9188479fc58eb6956106ea
Mira Makadia <patelmirak@gmail.com>
2022-01-19 10:47:17 -0500

    WIP:adds state to main and fetch data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom"
 import Profile from "../pages/Profile";
 import Index from "../pages/Index";
 import Home from "../pages/Home";
 
 function Main(props) {
+    const [item, setItem] = useState(null);
+
+const URL ="https://selldecor-backend.herokuapp.com/item";
+
+const getItem = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setItem(data);
+}
+
+useEffect(() => getItem(), [])
+
     return (
         <main>
             <Switch>
@@ -11,7 +24,7 @@ function Main(props) {
                     <Home />
                 </Route>
                 <Route path="/index">
-                    <Index />
+                    <Index item={item} />
                 </Route>
                 <Route path="/profile">
                     <Profile />
