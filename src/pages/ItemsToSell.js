1ec54f75a8107e38fe3a4576f372bad72a8cbc21
Mira Makadia <patelmirak@gmail.com>
2022-01-21 10:33:21 -0500

    WIP:formats itemsData to table

--- src/pages/ItemsToSell.js
+++ src/pages/ItemsToSell.js
@@ -7,26 +7,55 @@ function ItemsToSell(props) {
     const handleClick = (itm) => {
         props.deleteCheese(itm._id);
     }
-    
+
     const loaded = () => {
-        return (props.itemsData.map((item) => (
-            <div key={item._id} className="item">
-               <h3>{item.name}</h3>
-               <h3>{item.countryOfOrigin}</h3>
-               <button onClick={()=>props.handleUpdate(item)}>Update</button>
-               <button onClick={()=>handleClick(item)}>Delete</button>  
-            </div>)
-        ))
+        return <div>
+
+            <h1 className="indexHdr">Items to Sell</h1>
+            <div className="areaTable">
+                <table className="itemTable">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Category</th>
+                            <th>Image</th>
+                            <th>Price</th>
+                            <th>Settings</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.itemsData.map((item) => {
+                            const { name, category, img, price } = item;
+                            return (
+
+                                <tr key={item._id} className="tb-row">
+
+                                    <td>{name}</td>
+                                    <td>{category}</td>
+                                    <td><img src={img} alt={name} /></td>
+                                    <td>{price}</td>
+                                    <td>
+                                        <button onClick={() => props.handleUpdate(item)}>Update</button>
+                                        <button onClick={() => handleClick(item)}>Delete</button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
     }
-    
-    const loading =() => {
+
+    const loading = () => {
         return <h1>Loading ...</h1>
     }
-        return (
-           <section>
-                    {props.value ? <CreateForm {...props} /> : <UpdateForm {...props}/>}
-                {props.itemsData ? loaded() : loading()}
-            </section>
-        )
+    return (
+        <section>
+            {props.value ? <CreateForm {...props} /> : <UpdateForm {...props} />}
+            {props.itemsData ? loaded() : loading()}
+        </section>
+    )
 }
 export default ItemsToSell;
