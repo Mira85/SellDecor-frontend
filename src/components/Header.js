f10aabe527e169f4c83ac5750e65686ac158dca9
Mira Makadia <patelmirak@gmail.com>
2022-01-27 22:15:55 -0500

    adds style to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,39 +8,45 @@ function Header(props) {
     return (
         <nav className="nav">
             <div className="mainNav">
-            <Link to="/">
-                <div>Home</div>
-            </Link>
-            <Link to="/profile">
-                <div>Profile</div>
-            </Link>
-            {
-                props.user ?
-                <button onClick={logout}>Logout</button>
-                :
-                
-                <button onClick={login}>Login</button>
-            }
+                <div className="appName"><h1>SellDecor</h1>
+                </div>
+                <div className="navBtns">
+                    <Link to="/">
+                        <div>Home</div>
+                    </Link>
+                    <Link to="/profile">
+                        <div>Profile</div>
+                    </Link>
+                    {
+                        props.user ?
+                            <button onClick={logout}>Logout</button>
+                            :
+
+                            <button onClick={login}>Login</button>
+                    }
+                </div>
+
             </div>
             <div className="subNav">
-            <Link to="/collection">
-            <button onClick={()=> props.handleClickBtn("party")}>Party</button>
-            </Link>
-            <Link to="/collection">
-            <button onClick={()=> props.handleClickBtn("christmas")}>Christmas</button>
-            </Link>
-            <Link to="/collection">
-            <button onClick={()=> props.handleClickBtn("halloween")}>Halloween</button>
-            </Link>
-            <Link to="/collection">
-            <button onClick={()=> props.handleClickBtn("easter")}>Easter</button>
-            </Link>
-            <Link to="/collection">
-            <button onClick={()=> props.handleClickBtn("valentines day")}>Valentines Day</button>
-            </Link>
-                </div>
-               
-        
+                <Link to="/collection">
+                    <button onClick={() => props.handleClickBtn("party")}>Party</button>
+                </Link>
+                <Link to="/collection">
+                    <button onClick={() => props.handleClickBtn("christmas")}>Christmas</button>
+                </Link>
+                <Link to="/collection">
+                    <button onClick={() => props.handleClickBtn("halloween")}>Halloween</button>
+                </Link>
+                <Link to="/collection">
+                    <button onClick={() => props.handleClickBtn("easter")}>Easter</button>
+                </Link>
+                <Link to="/collection">
+                    <button onClick={() => props.handleClickBtn("valentines day")}>Valentines Day</button>
+                </Link>
+            </div>
+            <hr />
+
+
         </nav>
     )
 }
