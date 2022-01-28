5db502da6e87bc4a8cb15969e8c0edfd9a22ecb8
Mira Makadia <patelmirak@gmail.com>
2022-01-28 11:13:37 -0500

    adds background image to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,21 @@
+import "./Home.scss";
 function Home(props) {
-    return <h1>Home</h1>;
+    return (
+        <div className="homeContainer">
+        
+            <div className="homeBckGround" style={{ backgroundImage: "url(/images/homepic.jpg)"}}>
+                
+                <div className="homeBox">
+                    <div className="homeText">
+                        <h2>Occasion over ?</h2>
+                        Sell your decoration or buy other decor items at reasonable prices
+
+                    </div>
+
+                </div>
+            </div>
+            </div>
+    );
 }
 
 export default Home;
