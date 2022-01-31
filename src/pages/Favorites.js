fc651ccd5904b79b76e66d0282e29be34aa584e6
Mira Makadia <patelmirak@gmail.com>
2022-01-31 11:28:28 -0500

    adds delete icon and styles to fav item

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,19 +1,18 @@
+import { TiDeleteOutline } from "react-icons/ti";
 import "./Favorites.scss";
 
 function Favorites(props) {
     console.log('favorte props', props)
     return (
         <div className="favoriteContainer">
-    <h1>Favorites</h1>
            { props.favorites.map((item) => (
-        <div key={item._id} className="cartItem">
-                <h3>{item.name}</h3>
-           
-            <img src={item.img} alt={item.name} style={{
-                                    height: "6.125rem",
-                                    width: "6.125rem",
-                                }} />
-            <button onClick={()=>props.handleDeleteFavorite(item)} className="favoriteDeleteBtn">Delete</button>
+        <div key={item._id} className="favoriteItem">
+                
+           <div className="favImgBox">
+            <img src={item.img} alt={item.name} className="favoriteItemImg" />
+                                <div>{item.name}</div>
+                                </div>
+            <TiDeleteOutline onClick={()=>props.handleDeleteFavorite(item)} className="favoriteDeleteBtn" />
             
         </div>
 
